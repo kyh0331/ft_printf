8afbcf10ad32fa6a6a4ef3670691873d6c28357b
Yonghyun Kwon <ykwon@c5r7s1.42seoul.kr>
2021-03-16 20:38:27 +0900

    day 1

--- ft_printf.c
+++ ft_printf.c
@@ -1,2 +1,29 @@
 #include <stdarg.h>
 
+int check_flag()
+{
+	-
+	0
+	.
+	*
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		i;
+	int		inc_num;
+
+	i = 0;
+	while (format[i++] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			inc_num = check_flag();
+			i += inc_num;
+		}
+	}
+
+	va_start(ap, format);
+	va_end(ap);
+}
